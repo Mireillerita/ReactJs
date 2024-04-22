f8283aa1352d8e61273bdaa03c0aaf6ca86d204b
Mireillerita <mireillendahimana@gmail.com>
2024-04-22 00:27:04 -0700

    react portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+function App() {
+  return (
+   <div>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Project/>
+    <Contact/>
+   </div>
+  );
+}
+
+export default App
